79a5260fa76446df941e2bcc55a39e492d75f917
mmobrain <mmobrain@gmail.com>
2026-01-30 23:31:31 +0100

    update 1.1

--- DB/db.lua
+++ DB/db.lua
@@ -1,6 +1,6 @@
 _G.ETHTweaks_OptionalDB_Data = {
-version = "v1.0",
+version = "v1.1",
 changelog = "|cff00ff00• Starter Echo database.",
 
-data ="!EHT1!1WCYnaqmimu0gAoywINa9FHLOaC7j9TSc4QK47cGOGBRgBnLxs6KnBOhvI)OGZvpwNFit5m",
+data ="!EHT1!9izswmaimi0luxqgD4(FWQTcUlp9Nab1b8ySJpNce2g)kYgB7)ja3tG18kFjgtmMyg8klzrab7c2f8umprJltowSa0g9Gfz4co5jZNvPOWFWHGlbtpNh1VtEo3(LPfdTAxADIIfLAFgsuY0fhyaLpLlyAvpBMZq7UZDpS3wW2pwuTtJbJHzfCGN(LwCbBLZHPWSALHV9kfJMJjp7dLHlP14MpWuy(S6bMIkjsUxikLgSRc9DoDUuxmvd9hRB9rkzy6N394l",
 }
